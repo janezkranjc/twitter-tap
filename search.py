bb731948235ea6e2545c6354a178864cd4602aa0
Janez K <janez.kranjc@gmail.com>
2014-04-12 01:36:36 +0200

    added different loglevels and fixed bug with geocode and language queries

--- search.py
+++ search.py
@@ -6,10 +6,23 @@ from time import sleep
 import signal
 import pymongo
 import urlparse
+import logging
 
-from twython.exceptions import TwythonRateLimitError
+FORMAT = '[%(asctime)-15s] %(levelname)s: %(message)s'
+
+logging_dict = {
+    "DEBUG":logging.DEBUG,
+    "INFO":logging.INFO,
+    "WARN":logging.WARN,
+    "ERROR":logging.ERROR,
+    "CRITICAL":logging.CRITICAL,
+    "FATAL":logging.FATAL,
+}
+
+from twython.exceptions import TwythonRateLimitError, TwythonError
 
 def exit_gracefully(signal, frame):
+    logger.warn("SIGINT signal received! Shutting down.")
     sys.exit(0)
 
 signal.signal(signal.SIGINT, exit_gracefully)
@@ -27,6 +40,7 @@ parser.add_argument('-q', '--query', type=unicode, dest='query', required=True, 
 parser.add_argument('-g', '--geocode', type=unicode, dest='geocode', help='Returns tweets by users located within a given radius of the given latitude/longitude. The location is preferentially taking from the Geotagging API, but will fall back to their Twitter profile. The parameter value is specified by "latitude,longitude,radius", where radius units must be specified as either "mi" (miles) or "km" (kilometers). Note that you cannot use the near operator via the API to geocode arbitrary locations; however you can use this geocode parameter to search near geocodes directly. A maximum of 1,000 distinct "sub-regions" will be considered when using the radius modifier. Example value: 37.781157,-122.398720,1mi')
 parser.add_argument('-l', '--lang', type=unicode, dest='lang', help='Restricts tweets to the given language, given by an ISO 639-1 code. Language detection is best-effort.\nExample value: eu')
 parser.add_argument('-d', '--dbname', type=unicode, dest='dbname', default='twitter', help='Database name. Defaults to \'twitter\'.')
+parser.add_argument('-v', '--verbosity', type=unicode, dest='loglevel', default='WARN', choices=["DEBUG","INFO","WARN","ERROR","CRITICAL","FATAL"], help='The level of verbosity.')
 
 args = parser.parse_args()
 
@@ -34,13 +48,17 @@ query = args.query
 geocode = args.geocode
 lang = args.lang
 dbname = args.dbname
+loglevel = args.loglevel
+
+logging.basicConfig(format=FORMAT,level=logging_dict[loglevel])
+logger = logging.getLogger('twitter')
 
 # here we get the access token if it is not written in the settings
 if settings.ACCESS_TOKEN == '':
-    sys.stdout.write("No access token found in settings. Obtaining one now...\n")
+    logger.warn("No access token found in settings. Obtaining one now...")
     token_getter = Twython(settings.CONSUMER_KEY,settings.CONSUMER_SECRET,oauth_version=2)
     settings.ACCESS_TOKEN = token_getter.obtain_access_token()
-    sys.stdout.write("Access token: "+settings.ACCESS_TOKEN+"\n")
+    logger.warn("Access token: "+settings.ACCESS_TOKEN)
 
 twitter = Twython(settings.CONSUMER_KEY, access_token=settings.ACCESS_TOKEN)
 
@@ -51,10 +69,10 @@ db = client[dbname]
 queries = db.queries
 tweets = db.tweets
 
-queries.ensure_index("query",direction=pymongo.ASCENDING,unique=True)
+queries.ensure_index([("query",pymongo.ASCENDING),("geocode",pymongo.ASCENDING),("lang",pymongo.ASCENDING)],unique=True)
 tweets.ensure_index("id",direction=pymongo.DESCENDING,unique=True)
 
-current_query = queries.find_one({'query':query})
+current_query = queries.find_one({'query':query,'geocode':geocode,'lang':lang})
 if current_query:
     since_id = current_query['since_id']
 else:
@@ -66,17 +84,23 @@ def perform_query(**kwargs):
         try:
             results = twitter.search(**kwargs)
         except TwythonRateLimitError:
-            sys.stderr.write("Rate limit reached, taking a break for a minute...\n")
+            logger.warn("Rate limit reached, taking a break for a minute...\n")
             sleep(60)
             continue
-        except:
-            sys.stderr.write("Some other error occured, taking a break for half a minute...")
+        except TwythonError:
+            logger.error("Some other error occured, taking a break for half a minute...")
             sleep(30)
+            continue
         return results
 
 def save_tweets(statuses):
     for status in statuses:
         tweets.update({'id':status['id']},status,upsert=True)
+    if len(statuses)==0:
+        logger.debug("No new tweets. Taking a break for 10 seconds...")
+        sleep(10)
+    else:
+        logger.debug("Received "+str(len(statuses))+" tweets.")
 
 while True:
     results = perform_query(q=query,geocode=geocode,lang=lang,count=100,since_id=since_id)
@@ -84,9 +108,8 @@ while True:
     refresh_url = results['search_metadata'].get('refresh_url')
     p = urlparse.urlparse(refresh_url)
     new_since_id = dict(urlparse.parse_qsl(p.query))['since_id']
-    queries.update({'query':query},{"$set":{'since_id':new_since_id}},upsert=True)
-
-    #print results['http_headers']['x-rate-limit-remaining']
+    queries.update({'query':query,'geocode':geocode,'lang':lang},{"$set":{'since_id':new_since_id}},upsert=True)
+    logger.debug("Rate limit for current window: "+str(results['http_headers']['x-rate-limit-remaining']))
     #print len(results['statuses'])
     save_tweets(results['statuses'])
 
@@ -97,19 +120,9 @@ while True:
         results = perform_query(q=query,geocode=geocode,lang=lang,count=100,since_id=since_id,max_id=next_results_max_id)
         next_results = results['search_metadata'].get('next_results')
         #print results['http_headers']['x-rate-limit-remaining']
+        logger.debug("Rate limit for current window: "+str(results['http_headers']['x-rate-limit-remaining']))
         #print len(results['statuses'])
         save_tweets(results['statuses'])
 
 
     since_id = new_since_id
-
-"""
-if results.next_results:
-            p = urlparse.urlparse(results.next_results)
-            results.next_results_max_id = dict(urlparse.parse_qsl(p.query))['max_id']
-        for status in json['statuses']:
-            results.append(Status.parse(api, status))
-"""
-
-#for result in results['statuses']:
-#    print result['text']

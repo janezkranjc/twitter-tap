9c2546bdfeb3e7176c609d0b3c742e02af1a4a26
Janez K <janez.kranjc@gmail.com>
2014-04-11 18:52:05 +0200

    gracefully exit on SIGINT

--- search.py
+++ search.py
@@ -3,6 +3,12 @@
 import sys
 import argparse
 from time import sleep
+import signal
+
+def exit_gracefully(signal, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_gracefully)
 
 try:
     import settings
@@ -13,7 +19,7 @@ except:
 from twython import Twython
 
 parser = argparse.ArgumentParser(description='Search Twitter and return Tweets')
-parser.add_argument('-q', '--query', type=unicode, dest='query', required=True, help='A UTF-8 search query of 1,000 characters maximum, including operators. Queries may additionally be limited by complexity.')
+parser.add_argument('-q', '--query', type=unicode, dest='query', required=True, help='A UTF-8 search query of 1,000 characters maximum, including operators. Queries may additionally be limited by complexity. Information on how to construct a query is available at https://dev.twitter.com/docs/using-search')
 parser.add_argument('-g', '--geocode', type=unicode, dest='geocode', help='Returns tweets by users located within a given radius of the given latitude/longitude. The location is preferentially taking from the Geotagging API, but will fall back to their Twitter profile. The parameter value is specified by "latitude,longitude,radius", where radius units must be specified as either "mi" (miles) or "km" (kilometers). Note that you cannot use the near operator via the API to geocode arbitrary locations; however you can use this geocode parameter to search near geocodes directly. A maximum of 1,000 distinct "sub-regions" will be considered when using the radius modifier. Example value: 37.781157,-122.398720,1mi')
 parser.add_argument('-l', '--lang', type=unicode, dest='lang', help='Restricts tweets to the given language, given by an ISO 639-1 code. Language detection is best-effort.\nExample value: eu')
 

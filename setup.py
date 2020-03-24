d59645a7adb9fa95cb2430d587a4f4fc8cc6fe3e
Janez Kranjc <janez.kranjc@gmail.com>
2020-03-24 08:45:59 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 import os
+
 long_description = 'Twitter Tap is a python tool that connects to the Twitter API and issues calls to the search or the streaming endpoint using a query that the user has entered.'
 if os.path.exists('README.rst'):
     long_description = open('README.md').read()
@@ -18,21 +19,21 @@ CLASSIFIERS = [
 
 dist = setup(
     name='twitter-tap',
-    version='2.1.0',
+    version='2.2.0',
     author='Janez Kranjc',
     description='Collect tweets to a mongoDB using either the Twitter search API or the streaming API.',
     long_description=long_description,
     author_email='janez.kranjc@gmail.com',
     url='http://janezkranjc.github.io/twitter-tap/',
-    license = 'MIT',
-    install_requires=['pymongo','twython','six'],
+    license='MIT',
+    install_requires=['pymongo', 'twython', 'six'],
     classifiers=CLASSIFIERS,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     entry_points={
         'console_scripts': [
-         'tap = twitter_tap.tap:main',
+            'tap = twitter_tap.tap:main',
         ],
     }
 )
